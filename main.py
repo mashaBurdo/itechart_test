bf3175ebee484d69711a49012a65d0cf92f02551
maryia <masha.burdo@gmail.com>
2021-03-03 15:14:59 +0300

    before first attempt to test

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import logging
 from elasticsearch import Elasticsearch
 
 
+def get_json_from_db(query):
+    conn = sqlite3.connect('db.sqlite')
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+
+    rows = db.execute(query).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return [dict(ix) for ix in rows]
+
+
 def pretty_db():
     conn = sqlite3.connect('db.sqlite')
     # for row in conn.execute('select id, name from writers where name="N/A" '):
@@ -28,34 +41,39 @@ def pretty_db():
     cur.execute("update movies set director='None' where director='N/A'")
     conn.commit()
 
+    cur = conn.cursor()
+    cur.execute("update movies set imdb_rating='0' where imdb_rating='N/A'")
+    conn.commit()
+
     # for row in conn.execute('select title, director from movies where director="None"'):
     #     print('After N/A directors updating to None:', row)
 
     cur = conn.cursor()
     cur.execute('UPDATE movies SET writers = \'[{"id": "\' || writer || \'"}]\'  where writer != ""')
     conn.commit()
+
+
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS movie_writers (movie_id text NOT NULL, writer_id text NOT NULL)')
+    conn.commit()
+
+    writers_data = get_json_from_db('select writers, id  from movies')
+    for row  in writers_data:
+        movie_id = row["id"]
+        writers_str = row["writers"].replace('[','').replace(']','').replace('}','').replace('{','').replace('"','')
+        writers_list = [ {e.split(': ')[0]: e.split(': ')[1]} for e in writers_str.split(', ')]
+
+        for writer in writers_list:
+            data = (movie_id, writer["id"])
+            cur = conn.cursor()
+            cur.execute('INSERT INTO movie_writers(movie_id, writer_id) VALUES(?,?)', data)
+            conn.commit()
+
+
+
     conn.close()
 
     print('DB is pretty')
-
-def get_json_from_db(query):
-    conn = sqlite3.connect('db.sqlite')
-    conn.row_factory = sqlite3.Row
-    db = conn.cursor()
-
-    rows = db.execute(query).fetchall()
-
-    conn.commit()
-    conn.close()
-
-    return [dict(ix) for ix in rows]
-    # result = []
-    # for r in rows:
-    #     r = dict(r)
-    #     r['writers'] = list( r['writers'])
-    #     result.append(r)
-    # return result
-
 
 
 def connect_elasticsearch():
@@ -204,13 +222,12 @@ def store_record(elastic_object, index_name, record):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR)
-    pretty_db()
+    # pretty_db()
     es_obj = connect_elasticsearch()
-    a = create_index(es_obj)
-
+    # a = create_index(es_obj)
 
     pretty_json = get_json_from_db('select id, imdb_rating, genre, title, plot description,'
-                                   ' director from movies limit 3')
+                                   ' director from movies')
     for movie_data  in pretty_json:
         actor_query = f'select a.id, a.name from movie_actors ma join actors a on ma.actor_id = a.id where movie_id=\'{movie_data ["id"]}\''
         actors_data = get_json_from_db(actor_query)
@@ -219,8 +236,19 @@ if __name__ == '__main__':
         for actor in actors_data:
             actors_names += actor['name']+', '
 
+        writer_query = f'select w.id, w.name from movie_writers mw join writers w on mw.writer_id = w.id where movie_id=\'{movie_data["id"]}\''
+        writers_data = get_json_from_db(writer_query)
+
+        writers_names = ''
+        for writer in writers_data:
+            writers_names += writer['name'] + ', '
+
         movie_data['actors_names'] = actors_names
         movie_data['actors'] = actors_data
+        movie_data['writers_names'] = writers_names
+        movie_data['writers'] = writers_data
         movie_data['imdb_rating'] = float(movie_data['imdb_rating'] )
-        print(movie_data , end='\n\n')
+
+        # print(movie_data , end='\n\n')
+
         out = store_record(es_obj, 'movies', movie_data)
