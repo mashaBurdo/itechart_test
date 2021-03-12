b8fc0190193c7bd7e7a65bd7e1372abab7b09415
Mariya Burdo <masha.burdo@gmail.com>
2021-03-12 15:43:49 +0300

    db_sqlite is pretty. deleted columns from movies

--- schema_design/sqlite_db_pretty.py
+++ schema_design/sqlite_db_pretty.py
@@ -14,28 +14,27 @@ def get_data_from_db(query):
     return [dict(ix) for ix in rows]
 
 
-
 def make_db_pretty():
     conn = sqlite3.connect("db.sqlite")
     cur = conn.cursor()
 
-    # cur.execute("delete from actors where name='N/A'")
-    # cur.execute("delete from writers where name='N/A'")
-    # cur.execute("update movies set director = null where director='N/A'")
-    # cur.execute("update movies set plot = null where plot='N/A'")
-    # cur.execute("update movies set imdb_rating = '0' where imdb_rating='N/A'")
-    # cur.execute(
-    #     'UPDATE movies SET writers = \'[{"id": "\' || writer || \'"}]\'  where writer != ""'
-    # )
-    # cur.execute(
-    #     "CREATE TABLE IF NOT EXISTS movie_writers (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movie_id text NOT NULL, writer_id text NOT NULL)"
-    # )
-    # cur.execute(
-    #     "CREATE TABLE IF NOT EXISTS new_movie_actors (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movie_id text NOT NULL, actor_id text NOT NULL)"
-    # )
-    # cur.execute("INSERT INTO new_movie_actors (movie_id, actor_id) SELECT movie_id, actor_id FROM movie_actors")
-    # cur.execute("DROP TABLE IF EXISTS movie_actors")
-    # cur.execute("ALTER TABLE new_movie_actors RENAME TO movie_actors;")
+    cur.execute("delete from actors where name='N/A'")
+    cur.execute("delete from writers where name='N/A'")
+    cur.execute("update movies set director = null where director='N/A'")
+    cur.execute("update movies set plot = null where plot='N/A'")
+    cur.execute("update movies set imdb_rating = '0' where imdb_rating='N/A'")
+    cur.execute(
+        'UPDATE movies SET writers = \'[{"id": "\' || writer || \'"}]\'  where writer != ""'
+    )
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS movie_writers (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movie_id text NOT NULL, writer_id text NOT NULL)"
+    )
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS new_movie_actors (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movie_id text NOT NULL, actor_id text NOT NULL)"
+    )
+    cur.execute("INSERT INTO new_movie_actors (movie_id, actor_id) SELECT movie_id, actor_id FROM movie_actors")
+    cur.execute("DROP TABLE IF EXISTS movie_actors")
+    cur.execute("ALTER TABLE new_movie_actors RENAME TO movie_actors;")
 
     cur.execute(
         "CREATE TABLE IF NOT EXISTS movie_genres (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movie_id text NOT NULL, genre_id text NOT NULL)"
@@ -44,6 +43,7 @@ def make_db_pretty():
         "CREATE TABLE IF NOT EXISTS genres (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, genre text NOT NULL)"
     )
     conn.commit()
+
 
     genres_data = get_data_from_db("select genre, id  from movies")
     for row in genres_data:
@@ -57,37 +57,43 @@ def make_db_pretty():
             cur.execute("INSERT INTO movie_genres (genre_id, movie_id) VALUES(?, ?)", (genre_exists[0]['id'], row['id']))
             conn.commit()
 
+    writers_data = get_data_from_db("select writers, id  from movies")
+    movie_actors = cur.execute("select * from movie_writers limit 1").fetchall()
+    if not movie_actors:
+        for row in writers_data:
+            movie_id = row["id"]
+            writers_str = (
+                row["writers"]
+                .replace("[", "")
+                .replace("]", "")
+                .replace("}", "")
+                .replace("{", "")
+                .replace('"', "")
+            )
+            writers_list = [
+                {e.split(": ")[0]: e.split(": ")[1]} for e in writers_str.split(", ")
+            ]
 
+            cur = conn.cursor()
+            for writer in writers_list:
+                data = (movie_id, writer["id"])
+                cur.execute(
+                    "INSERT INTO movie_writers(movie_id, writer_id) VALUES(?,?)", data
+                )
+            conn.commit()
+            # print("Movie actors updated")
 
-    #
-    # writers_data = get_data_from_db("select writers, id  from movies")
-    # movie_actors = cur.execute("select * from movie_writers limit 1").fetchall()
-    # if not movie_actors:
-    #     for row in writers_data:
-    #         movie_id = row["id"]
-    #         writers_str = (
-    #             row["writers"]
-    #             .replace("[", "")
-    #             .replace("]", "")
-    #             .replace("}", "")
-    #             .replace("{", "")
-    #             .replace('"', "")
-    #         )
-    #         writers_list = [
-    #             {e.split(": ")[0]: e.split(": ")[1]} for e in writers_str.split(", ")
-    #         ]
-    #
-    #         cur = conn.cursor()
-    #         for writer in writers_list:
-    #             data = (movie_id, writer["id"])
-    #             cur.execute(
-    #                 "INSERT INTO movie_writers(movie_id, writer_id) VALUES(?,?)", data
-    #             )
-    #         conn.commit()
-    #     print("Movie actors updated")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS new_movies (id PRIMARY KEY NOT NULL, director text, title text, description text , rating text)"
+    )
+    cur.execute(
+        "INSERT INTO new_movies (id, director, title, description, rating) SELECT id, director, title, plot, imdb_rating FROM movies"
+    )
+    cur.execute("DROP TABLE IF EXISTS movies")
+    cur.execute("ALTER TABLE new_movies RENAME TO movies;")
+    conn.commit()
 
-
-
+    print("Sqlite database is pretty!")
     conn.close()
 
 
