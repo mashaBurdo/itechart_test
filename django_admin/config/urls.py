9145fbcd57c9122909a49328c3338c1731943fc8
maryia <masha.burdo@gmail.com>
2021-03-18 10:05:00 +0300

    refactoring of project file structure in progress

--- django_admin/config/urls.py
+++ django_admin/config/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('movies.urls')),
+    path('api/', include('api.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-
 ]
