4a8ce7511f73ac7d114ea1c9612eac0931d4b433
maryia <masha.burdo@gmail.com>
2021-03-18 12:20:27 +0300

    api implementation in progress

--- django_admin/api/v1/views.py
+++ django_admin/api/v1/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from django.views import View
 
 
+
+
 class MoviesListApi(View):
     http_method_names = ['get']
 
---
+++ django_admin/data_transfer/pg_table_create_queries.py
@@ -0,0 +1,43 @@
+create_queries = [
+    """CREATE TABLE IF NOT EXISTS film_work (
+id uuid PRIMARY KEY,
+title TEXT NOT NULL,
+description TEXT,
+creation_date DATE,
+certificate TEXT,
+file_path TEXT,
+rating FLOAT,
+old_id TEXT,
+type TEXT,
+created_at timestamp with time zone,
+updated_at timestamp with time zone
+)""",
+    """CREATE TABLE IF NOT EXISTS genre (
+id uuid PRIMARY KEY,
+name TEXT NOT NULL,
+old_id TEXT,
+description TEXT, 
+created_at timestamp with time zone, 
+updated_at timestamp with time zone
+)""",
+    """CREATE TABLE IF NOT EXISTS genre_film_work (
+id uuid PRIMARY KEY,
+film_work_id uuid NOT NULL,
+genre_id uuid NOT NULL,
+created_at timestamp with time zone
+)""",
+    """CREATE TABLE person (
+id uuid PRIMARY KEY,
+name TEXT NOT NULL,
+birth_date DATE,
+created_at timestamp with time zone,
+updated_at timestamp with time zone
+)""",
+    """CREATE TABLE IF NOT EXISTS person_film_work (
+id uuid PRIMARY KEY,
+film_work_id uuid NOT NULL,
+person_id uuid NOT NULL,
+role TEXT NOT NULL,
+created_at timestamp with time zone
+)""",
+]
