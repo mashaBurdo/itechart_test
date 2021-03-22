1bb3d7ba9d52be80a9ed931cee6719daf315548f
Mariya Burdo <masha.burdo@gmail.com>
2021-03-22 10:43:35 +0300

    cyclic iterator implemented

---
+++ ETL/cyclic_iterator.py
@@ -0,0 +1,25 @@
+import time
+
+
+class CyclicIterator:
+    def __init__(self, iterable):
+        self.restart = iterable
+        self.iterable = iter(iterable)
+
+    def __iter__(self):
+        self.iterable
+        return self
+
+    def __next__(self):
+        while True:
+            try:
+                item = next(self.iterable)
+                return item
+            except StopIteration:
+                self.__init__(self.restart)
+
+
+cyclic_iterator = CyclicIterator(['a','b','c'])
+for i in cyclic_iterator:
+    print(i)
+    time.sleep(0.5)
