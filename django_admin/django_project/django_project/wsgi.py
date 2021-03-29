f839a357779e97d558ee5fbafe146d22ae54457a
Mariya Burdo <masha.burdo@gmail.com>
2021-03-29 15:28:46 +0300

    debug toolbar fixed, shell_plus installed

--- django_admin/django_project/django_project/wsgi.py
+++ django_admin/django_project/django_project/wsgi.py
@@ -13,11 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 
 application = get_wsgi_application()
 
-# application = Cling(get_wsgi_application())
