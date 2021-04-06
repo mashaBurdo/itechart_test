b46ab3e6db6d83e89a338a4b52fe5996ce32cd70
maryia <masha.burdo@gmail.com>
2021-04-06 12:03:27 +0300

    signals for films do work

--- Django_app/django_project/django_project/wsgi.py
+++ Django_app/django_project/django_project/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 
 from django.core.wsgi import get_wsgi_application
 
--- ETL2/indexer/ETL_mechanism.py
+++ ETL2/indexer/ETL_mechanism.py
@@ -70,3 +70,5 @@ if __name__ == "__main__":
 
     final_state = State()
     final_state.clear_state()
+
+
