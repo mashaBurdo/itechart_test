a21d4e46d3093958bf51addddb640c266a9629cc
Mariya Burdo <masha.burdo@gmail.com>
2021-04-06 15:31:11 +0300

    genre saving signals completed

--- Django_app/django_project/movies/models.py
+++ Django_app/django_project/movies/models.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
 
+
 class Genre(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(_("name"), max_length=255)
--- Django_app/django_project/movies/signals.py
+++ Django_app/django_project/movies/signals.py
@@ -4,14 +4,38 @@ from elasticsearch import Elasticsearch
 from movies.models import (FilmWork, Genre, GenreFilmWork, Person,
                            PersonFilmWork)
 
-from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from django.contrib.postgres.aggregates.general import ArrayAgg
+from django.core.paginator import Paginator
+from django.db.models import F, Q, Case, Value, When, CharField
 
 
-def genres_changed(sender, **kwargs):
-    print(sender)
+ES_HOST = 'localhost'
+
+def genre_film_work_save(sender, instance, **kwargs):
+    film_id = instance.film_work.id
+    genres = FilmWork.objects.values("id").annotate(
+                    genres=ArrayAgg("genres__name", distinct=True),
+                    ).get(id=film_id)['genres']
+    script = f"ctx._source.genre = {genres}"
+    query = {
+         "script": {
+            "source": script,
+            "lang": "painless"
+         },
+         "query": {
+            "match": {
+                "id": film_id
+            }
+         }
+    }
+    print(query)
+    es_obj = Elasticsearch(hosts=[{"host": ES_HOST}], retry_on_timeout=True)
+    es_obj.update_by_query(body=query, index='movies')
 
 
-m2m_changed.connect(genres_changed, sender=FilmWork.genres.through)
+post_save.connect(genre_film_work_save, GenreFilmWork)
 
 
 def post_film_work_save(sender, instance, **kwargs):
@@ -20,9 +44,10 @@ def post_film_work_save(sender, instance, **kwargs):
         "title": instance.title,
         "imdb_rating": instance.rating,
         "description": instance.description,
+        "genre": [],
     }
 
-    es_obj = Elasticsearch(hosts=[{"host": "elasticsearch"}], retry_on_timeout=True)
+    es_obj = Elasticsearch(hosts=[{"host": ES_HOST}], retry_on_timeout=True)
     es_obj.index(index="movies", body=record)
 
 
@@ -32,7 +57,7 @@ post_save.connect(post_film_work_save, FilmWork)
 def pre_film_work_delete(sender, instance, **kwargs):
     id = instance.id
 
-    es_obj = Elasticsearch(hosts=[{"host": "elasticsearch"}], retry_on_timeout=True)
+    es_obj = Elasticsearch(hosts=[{"host": ES_HOST}], retry_on_timeout=True)
     es_obj.delete_by_query(index="movies", body={"query": {"match": {"id": id}}})
 
 
